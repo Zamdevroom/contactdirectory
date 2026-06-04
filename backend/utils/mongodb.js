af0d567ac455d61ca290fd93f1a5bbaa72e178aa
Farhan Baloch <farhan.baloch9632@gmail.com>
2026-06-04 23:22:58 +0500

    Change Password of testuser

--- backend/utils/mongodb.js
+++ backend/utils/mongodb.js
@@ -4,9 +4,7 @@ import dotenv from "dotenv";
 const connect = async () => {
     try {
         console.log("Connecting to database...");
-        const conn = await mongoose.connect(
-            "mongodb+srv://testuser:test123@contactdirectory.4uybg87.mongodb.net/contactdirectory?retryWrites=true&w=majority"
-        );
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected successfully.");
         return conn;
     } catch (error) {
